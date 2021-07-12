3ac9faa952c99815ad03242f42a4ef272b095448
HarishaS25 <harishsnaidu2000@gmail.com>
2021-07-12 07:05:55 +0530

    Added password reset feature

--- Sem6project/settings.py
+++ Sem6project/settings.py
@@ -139,3 +139,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'taskmanager-tasks'
 LOGIN_URL = 'login'
+
+EMAIL_HOST_USER="harishsnaidu2000@gmail.com"
+EMAIL_HOST_PASSWORD="yotgfqmgpwiraeid"
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+
